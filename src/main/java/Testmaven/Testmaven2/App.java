b16bb57f0ea2f136be85cb112c5dc9ea81772c58
Mehdi Gourija <m.gourija@gmail.com>
2021-09-16 13:06:59 +0100

    first comiit

---
+++ src/main/java/Testmaven/Testmaven2/App.java
@@ -0,0 +1,13 @@
+package Testmaven.Testmaven2;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
